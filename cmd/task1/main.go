331804649d6352d78e59d8be2a76e87ef13583f3
Hrvoje Seketa <hrvoje.seketa@heimgard.com>
2022-07-12 18:49:27 +0000

    added example in task1 main.go on how to test

--- cmd/task1/main.go
+++ cmd/task1/main.go
@@ -1,4 +1,10 @@
 package main
 
+import "../../pkg/fileio"
+
 func main() {
+
+	// example for how to test quickly
+	// NOTE: data does not have to be string, change it how you like
+	fileio.SaveFile("user", "file", "data")
 }
